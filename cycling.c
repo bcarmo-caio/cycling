f1a1e2ba6e6c3f7c8b34bdc0f97e7eb03992b1ab
user <user@shiny>
2015-04-18 18:07:13 -0300

    commmit test and minor changes

--- cycling.c
+++ cycling.c
@@ -8,6 +8,7 @@ struct runway_position {
 	int position[4];
 };
 
+//thread for a cyclist
 void *cyclist(void *arg) {
 	return NULL;
 }
@@ -18,15 +19,26 @@ int main(int argc, char **argv) {
 	struct runway_position *runway;
 	pthread_t *threads;
 
+	/*d := track length*/
 	d = atoi(argv[1]);
+
+	/*n := number of cyclists*/
 	n = atoi(argv[2]);
+
+	/*c := type of simulation
+	 *c = 'v' means cyclists may change their speed from 25km/h to 50km/h
+	 *	and vice-versa.
+	 *c = 'u' means cyclist may NOT change their speed, and the speed is
+	 *  a constant of 25km/h
+	 */
 	c = argv[3][0];
+
 	if (argc == 5 && strcmp(argv[4], "-d") == 0) debug_flag = 1; 
 
-	runway = malloc(d*sizeof(struct runway_position));
-	threads = malloc(n*sizeof(pthread_t));
+	runway = malloc(d * sizeof(struct runway_position));
+	threads = malloc(n * sizeof(pthread_t));
 	for (i = 0; i < n; i++)
-		pthread_create(&threads[i], NULL, &cyclist, NULL);
+		pthread_create(threads + i, NULL, &cyclist, NULL);
 
 	while (run_not_over) {
 		simulate_lap(lap_number);
@@ -37,16 +49,16 @@ int main(int argc, char **argv) {
 
 simulate_lap(lap_number) {
 	if (debug_flag && lap_number % 200 == 0) {
-		// debug
+		/* debug */
 		debug();
 	}
 	if (lap_number % 4 == 0) {
-		// breaking
+		/* breaking */
 		breaking();
 		update_positioning_data();
 	}
 	if (lap_number % 2 == 0) {
-		// elimination
+		/* elimination */
 		while (lap_not_over) {
 			simulate_72ms();
 			pthread_barrier_wait(72ms_barrier);
@@ -54,14 +66,14 @@ simulate_lap(lap_number) {
 	}
 }
 
-simulate_72ms() {
+void simulate_72ms(void) {
 }
 
-breaking() {
+void breaking(void) {
 }
 
-update_positioning_data() {
+void update_positioning_data(void) {
 }
 
-debug() {
+void debug(void) {
 }
